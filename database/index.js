220c81cfea86c0c9c040f1cb2a4457d9fff56e9d
afidenigeria <afidenigeria@gmail.com>
2021-12-22 03:00:50 +0100

    Setup responseHelper, logger and encryption helper classes

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const connection = mongoose.connect("mongodb://localhost:27017/shopping-cart", {
+const connection = mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(() => console.log("Connect Successful"))
@@ -9,4 +9,4 @@ const connection = mongoose.connect("mongodb://localhost:27017/shopping-cart", {
 
 
 
-module.exports = connection
+module.exports = connection;
